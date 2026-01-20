de9357bfd664b3db058cf1768d3fc53e536bb09e
guifischman <guifischman@hotmail.com>
2026-01-20 10:20:25 -0400

    Add files via upload

---
+++ src/main/java/com/company/artist_album_api/ArtistAlbumApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.artist_album_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistAlbumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistAlbumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/artist_album_api/ArtistAlbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.artist_album_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistAlbumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
